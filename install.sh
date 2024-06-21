bccc3b0f5a4f4c6770686265e0804b369a64b66e
Team Codelinsoft <specialworld83@gmail.com>
2024-06-21 17:34:19 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo mkdir /usr/share/icons/convert_pdf
-sudo cp -rv convert_pdf /usr/share/icons/
-sudo cp -rv convert_pdf.desktop ~/.local/share/kio/servicemenus/
-echo "Installation successifully"
