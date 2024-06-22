9e08fc923087625569fac099c5d88f084b2ce9a0
Team Codelinsoft <specialworld83@gmail.com>
2024-06-22 19:06:34 +0200

    Update install.sh
    Fin installer permission

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo mkdir /usr/share/icons/convert_pdf
 sudo cp -rv icon_jpg.png icon_png.png odp.png odt.png powerpoint_2007.png word_2007.png /usr/share/icons/convert_pdf/
 cp -rv convert_pdf.desktop ~/.local/share/kio/servicemenus/
+chmod 775 ~/.local/share/kio/servicemenus/convert_pdf.desktop
 echo "Installation successifully"
